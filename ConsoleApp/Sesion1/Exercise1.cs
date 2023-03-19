468551344d1fefa7e50740e0f51f83b4bc2fbb73
Philip Perez <jperez.c92@gmail.com>
2023-03-19 17:56:52 -0500

    sesion1

---
+++ ConsoleApp/Sesion1/Exercise1.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class Exercise1
+{
+
+    public void execute()
+    {
+        Console.WriteLine("Write your name");
+
+        string? name = Console.ReadLine();
+
+        string value = name is not null && name.Count() > 0 ? name : "world";
+
+        Console.WriteLine($"Hello {value}");
+    }
+
+}
+
---
+++ ConsoleApp/Sesion1/Exercise2.cs
@@ -0,0 +1,11 @@
+﻿class Exercise2
+{
+
+    public void execute()
+    {
+        Console.WriteLine($"Current Time: {DateTime.UtcNow.TimeOfDay.Hours}:{DateTime.UtcNow.TimeOfDay.Minutes}");
+
+    }
+
+}
+
